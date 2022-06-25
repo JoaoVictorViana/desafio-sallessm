95951e4051b0d09f9f48ea1cf6922e2f0c830c4c
João Victor Viana <jv.duarte.viana@gmail.com>
2022-06-24 21:23:14 -0300

    test: husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
